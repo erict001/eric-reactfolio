3b1bb78166a7735fc0b92883985da0be3dd7b525
Eric Taylor <eric.taylor1012@gmail.com>
2023-03-28 19:10:52 -0700

    center headers in mobile responsive view

---
+++ src/utils/accordionData.js
@@ -0,0 +1,27 @@
+export const accordionData = [
+    {
+        title: "Full Stack Application Developer",
+        company: "Cappers Sports",
+        description: ""
+    },
+    {
+        title: "Full Stack Web Developer Teaching Assistant",
+        company: "University of Oregon",
+        description: ""
+    },
+    {
+        title: "Founder, CEO",
+        company: "Apollo Academy",
+        description: ""
+    },
+    {
+        title: "Co-Founder, COO",
+        company: "Fenix Experience",
+        description: ""
+    },
+    {
+        title: "Director of Operations",
+        company: "City Baseball",
+        description: ""
+    }
+]
