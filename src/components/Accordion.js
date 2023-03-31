e180b864de82c7c959443bfd4be11b2bb3348ca5
Eric Taylor <eric.taylor1012@gmail.com>
2023-03-31 14:05:47 -0700

    accordion working

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,34 @@ import React, { useState } from 'react';
 const Accordion = ({ title, company, description1, description2, description3 }) => {
   const [isActive, setIsActive] = useState(false);
 
+  const styles = {
+    accordions: {
+      display: "flex",
+      width: "98.8%",
+      margin: "0 2vw 0 1vw",
+      border: "1px solid black",
+    },
+    header: {
+      display: "flex",
+      alignItems: "center"
+    }
+  }
+
   return (
     <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{title}</div>
-        <div>{company}</div>
-        <div>{isActive ? '-' : '+'}</div>
+      <div className="accordion-title" style={styles.accordions} onClick={() => setIsActive(!isActive)}>
+        <div class="max-w w-full lg:max-w-full lg:flex">
+          <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            <div class="mb-8">
+              <p class="text-sm text-gray-600 flex items-center">
+                Members only
+              </p>
+              <div class="text-gray-900 font-bold text-xl mb-2">{title}</div>
+              <p class="text-gray-700 text-base">{company}</p>
+            </div>
+            <div>{isActive ? '-' : 'Learn More'}</div>
+          </div>
+        </div>
       </div>
       {isActive && <div className="accordion-content">{description1}</div>}
       {isActive && <div className="accordion-content">{description2}</div>}
--- src/components/BuildAccordion.js
+++ src/components/BuildAccordion.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Accordion from './Accordion.js';
-import { accordionData } from '../utils/accordionData.js';
+import React from "react";
+import { accordionData } from "../utils/accordionData";
+import Accordion from "./Accordion.js"
 
 const BuildAccordion = () => {
+ 
   return (
     <div>
-      <h1>React Accordion Demo</h1>
       <div className="accordion">
         {accordionData.map(({ title, company, description1, description2, description3 }) => (
           <Accordion title={title} company={company} description1={description1} description2={description2} description3={description3}/>
@@ -13,6 +13,6 @@ const BuildAccordion = () => {
       </div>
     </div>
   );
-};
+}
 
 export default BuildAccordion;
