c28cbaffc505b2f77b3e0b0f682a49b2b1ad35cc
Eric Taylor <eric.taylor1012@gmail.com>
2023-04-03 20:23:49 -0700

    about section header now mobile responsive

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,10 +6,10 @@ class AboutSection extends Component {
             <div>
                 <div style={{ height: "auto", width: "100vw", backgroundColor: "#d7c25e" }}>
                     <div class="flex flex-row justify-center content-center pb-6 ml-3 mr-3">
-                        <div class="flex flex-row h-full w-full justify-around content-center pt-6">
-                            <h1 class="text-3xl text-black w-1/3 flex md:justify-center content-center font-bold">UI/UX DESIGNER</h1>
-                            <h1 class="text-3xl text-white w-1/3 flex md:justify-center font-bold">FULL STACK DEVELOPER</h1>
-                            <h1 class="text-3xl text-black w-1/3 flex md:justify-center font-bold">BUSINESS ADMINISTRATOR</h1>
+                        <div class="flex md:flex-row justify-center h-full w-full justify-around content-center pt-6 xs:flex-col">
+                            <h1 class="text-3xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen">UI/UX DESIGNER</h1>
+                            <h1 class="text-3xl text-white md:w-1/3 flex md:justify-center font-bold xs:w-screen">FULL STACK DEVELOPER</h1>
+                            <h1 class="text-3xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen">BUSINESS ADMINISTRATOR</h1>
                         </div>
                     </div>
                 </div>
