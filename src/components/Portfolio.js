1ff36572fedd22f5adde4ca461476e10e603cdaf
Eric Taylor <eric.taylor1012@gmail.com>
2023-04-05 21:39:15 -0700

    professional projects mobile responsive on larger screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -39,7 +39,7 @@ class Portfolio extends Component {
               <br></br>
               <br></br>
             </div>
-            <div class="flex justify-center flex-wrap xs:flex-col mx-2">
+            <div class="xl:columns-3 lg:columns-1  md:columns-1 sm:columns-1 sm:mx-4 xl:ml-4">
               <div class="md:max-w-5xl sm:max-w-3xl rounded overflow-hidden shadow-2xl mb-4 border-solid border-2 border-black">
                 <img class="w-full" src={apollo} alt="Apollo Academy website" />
                 <div class="px-6 py-4">
