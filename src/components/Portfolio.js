9ef7cd5ac460706891dd6660bb3b5ce2c424047e
Eric Taylor <eric.taylor1012@gmail.com>
2023-04-06 07:20:10 -0700

    trying flex instead of columns

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -38,7 +38,7 @@ class Portfolio extends Component {
               <br></br>
               <br></br>
             </div>
-            <div class="xl:columns-3 inline-block lg:columns-1  md:columns-1 sm:columns-1 sm:mx-4 xl:ml-4">
+            <div class="xl:flex space-x-4 lg:flex-col xl:mx-4 xl:ml-4">
               <div class="md:max-w-5xl sm:max-w-3xl rounded shadow-2xl mb-4 border-solid border-2 border-black">
                 <img class="w-full" src={apollo} alt="Apollo Academy website" />
                 <div class="px-6 py-4">
@@ -100,7 +100,7 @@ class Portfolio extends Component {
               <br></br>
               <br></br>
               <br></br>
-              <div class="xl:columns-3 inline-block lg:columns-1  md:columns-1 sm:columns-1 sm:mx-4 xl:ml-4">
+              <div class="xl:flex space-x-4 lg:flex-col xl:mx-4 xl:ml-4">
                 <div class="md:max-w-5xl sm:max-w-3xl rounded overflow-hidden shadow-2xl mb-4 border-solid border-2 border-black">
                   <img class="w-full" src={jeopardy} alt="Inspirational Jeopardy" />
                   <div class="px-6 py-4">
