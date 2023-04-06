7da383162fe8d08ed107839cac4e9249b411ed3a
Eric Taylor <eric.taylor1012@gmail.com>
2023-04-05 21:36:32 -0700

    about headers text resized

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,9 +7,9 @@ class AboutSection extends Component {
                 <div style={{ height: "auto", width: "100vw", backgroundColor: "#d7c25e" }}>
                     <div class="flex flex-row justify-center content-center pb-6 ml-3 mr-3">
                         <div class="flex md:flex-row justify-center h-full w-full justify-around content-center pt-6 xs:flex-col">
-                            <h1 class="text-3xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">UI/UX DESIGNER</h1>
-                            <h1 class="text-3xl text-white md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">FULL STACK DEVELOPER</h1>
-                            <h1 class="text-3xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">BUSINESS ADMINISTRATOR</h1>
+                            <h1 class="text-2xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">UI/UX DESIGNER</h1>
+                            <h1 class="text-2xl text-white md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">FULL STACK DEVELOPER</h1>
+                            <h1 class="text-2xl text-black md:w-1/3 flex md:justify-center font-bold xs:w-screen xs:justify-center">BUSINESS ADMINISTRATOR</h1>
                         </div>
                     </div>
                 </div>
